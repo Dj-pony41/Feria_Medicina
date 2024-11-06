40f0c20bec3739d971731c354743a015c40dd6be
julio <sfj0025410@est.univalle.edu>
2024-11-06 15:45:21 -0400

    Nuevas 2 Armas Anticuerpos y Arco de Toxinas

---
+++ Assets/01_Script/Projectile.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float speed = 500f;     private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(transform.forward * speed);         Destroy(gameObject, 5f);     }
+
+    void OnTriggerEnter(Collider other)
+    {
+                if (other.CompareTag("Player"))
+        {
+                        SlowEffect slowEffect = other.GetComponent<SlowEffect>();
+            if (slowEffect != null)
+            {
+                Debug.Log("Proyectil impactó al player, aplicando ralentización.");                 slowEffect.ApplySlowEffect();             }
+            Destroy(gameObject);         }
+        else if (!other.CompareTag("Weapon"))
+        {
+                        Destroy(gameObject);
+        }
+    }
+}
