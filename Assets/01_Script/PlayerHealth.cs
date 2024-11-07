d325163c8714fa55c787224b13066bf821f21e7d
Dj-pony41 <163031918+Dj-pony41@users.noreply.github.com>
2024-11-07 15:28:22 -0400

    ha

--- Assets/01_Script/PlayerHealth.cs
+++ Assets/01_Script/PlayerHealth.cs
@@ -6,6 +6,9 @@ public class PlayerHealth : MonoBehaviour
 {
     public float health = 100f;
 
+    // Objeto que desaparecerá al morir (arrástralo en el editor)
+    public GameObject objectToDestroy;
+
     public void TakeDamage(float amount)
     {
         health -= amount;
@@ -18,5 +21,14 @@ public class PlayerHealth : MonoBehaviour
     void Die()
     {
         Debug.Log("Player ha muerto");
-            }
+
+        if (objectToDestroy != null)
+        {
+            Destroy(objectToDestroy);
+        }
+        else
+        {
+            Debug.LogWarning("No se ha asignado el objeto a destruir.");
+        }
+    }
 }
